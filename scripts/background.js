5d0f23bf6e5ddcab008aeb451807b768de44043c
Adithya <nkvadithya2@gmail.com>
2025-05-18 23:00:32 +0530

    better structure

--- scripts/background.js
+++ scripts/background.js
@@ -12,14 +12,14 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
       tabId: tabId,
       path: isSupportedSite
         ? {
-            "16": "icons/icon16.png",
-            "48": "icons/icon48.png",
-            "128": "icons/icon128.png"
+            "16": "../icons/icon16.png",
+            "48": "../icons/icon48.png",
+            "128": "../icons/icon128.png"
           }
         : {
-            "16": "icons/icon16_inactive.png",
-            "48": "icons/icon48_inactive.png",
-            "128": "icons/icon128_inactive.png"
+            "16": "../icons/icon16_inactive.png",
+            "48": "../icons/icon48_inactive.png",
+            "128": "../icons/icon128_inactive.png"
           }
     });
   }
